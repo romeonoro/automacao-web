63d3c9e8d41ac9e507be402e4f96ca58d87a0c6c
Romeo Noro Guterres <romeo.noro@ufn.edu.br>
2025-06-16 22:39:23 -0300

    Update LogEntryRepository.java

--- automacao-web/src/main/java/com/automacao/automacaoweb/repository/LogEntryRepository.java
+++ automacao-web/src/main/java/com/automacao/automacaoweb/repository/LogEntryRepository.java
@@ -9,7 +9,4 @@ import java.util.List;
 
 @Repository // Marca esta interface como um componente de repositório
 public interface LogEntryRepository extends JpaRepository<LogEntry, Long> {
-    // Spring Data JPA fornecerá automaticamente métodos como save(), findById(), findAll(), deleteById(), etc.
-    // Você pode adicionar métodos de consulta personalizados aqui se precisar, como:
-     //List<LogEntry> findByAction(String action);
 }
