e6d5823306abc079c5b66ffdcc6e72803116c74c
Romeo Noro Guterres <romeo.noro@ufn.edu.br>
2025-06-16 22:41:50 -0300

    Update LedService.java

--- automacao-web/src/main/java/com/automacao/automacaoweb/service/LedService.java
+++ automacao-web/src/main/java/com/automacao/automacaoweb/service/LedService.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException; // Importar IOException
+import java.io.IOException; 
 
 @Service
 public class LedService {
